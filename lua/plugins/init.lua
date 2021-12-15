09337103c1dc8290edced6090907c5a181f68bca
JjersaR <86206596+JjersaR@users.noreply.github.com>
2021-12-15 10:56:35 -0600

    Update init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -83,5 +83,18 @@ return require('packer').startup(function()
 		
   --ranger
   use 'kevinhwang91/rnvimr'
+		
+    --problemas
+  use {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
 
 end)
