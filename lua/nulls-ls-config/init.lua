a06c77701888d5956515adade038a79ce177d179
JjersaR <86206596+JjersaR@users.noreply.github.com>
2021-12-27 19:11:37 -0600

    Create init.lua

---
+++ lua/nulls-ls-config/init.lua
@@ -0,0 +1,24 @@
+local null_ls = require("null-ls")
+
+local formatting = null_ls.builtins.formatting
+
+local sources = {
+  formatting.black, -- "python"
+  formatting.clang_format, -- "c", "cpp", "cs", "java"
+  formatting.eslint_d, -- "javascript", "javascriptreact", "typescript", "typescriptreact", "vue"
+  formatting.json_tool, -- "json"
+  formatting.lua_format, -- "lua"
+  formatting.perltidy, -- "perl"
+  formatting.phpcsfixer, --  "php"
+  formatting.prettier, -- "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "css", "scss", "less", "html"
+  formatting.sqlformat, -- "sql"
+}
+
+null_ls.setup({
+  sources = sources,
+  on_attach = function(client)
+    if client.resolved_capabilities.document_formatting then
+      vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+    end
+  end,
+})
