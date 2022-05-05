328ff63f220a88e82cce079b9252b96972fd066d
JjersaR <86206596+JjersaR@users.noreply.github.com>
2022-05-05 12:25:45 -0500

    Update init.lua

--- lua/whichkeys-config/init.lua
+++ lua/whichkeys-config/init.lua
@@ -58,6 +58,16 @@ local mappings = {
     R = {":CphReceive<CR>", "Receive"},
     s = {":CphStop<CR>", "Parar"},
   },
+  p = {
+    name = "Packer",
+    r = { ":PackerClean<cr>", "Remove Unused Plugins" },
+    c = { ":PackerCompile profile=true<cr>", "Recompile Plugins" },
+    i = { ":PackerInstall<cr>", "Install Plugins" },
+    p = { ":PackerProfile<cr>", "Packer Profile" },
+    s = { ":PackerSync<cr>", "Sync Plugins" },
+    S = { ":PackerStatus<cr>", "Packer Status" },
+    u = { ":PackerUpdate<cr>", "Update Plugins" }
+  },
 }
 local opts = {prefix = '<leader>'}
 
