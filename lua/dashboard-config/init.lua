5587ad3ede0d3d8d1cc97aade826f0037699efa4
JjersaR <86206596+JjersaR@users.noreply.github.com>
2022-05-12 20:23:14 -0500

    Update init.lua

--- lua/dashboard-config/init.lua
+++ lua/dashboard-config/init.lua
@@ -2,47 +2,41 @@ vim.g.dashboard_default_executive = 'telescope'
 
 vim.g.dashboard_custom_header = {
 
-  [[]],
-  [[]],
-  [[]],
-  [[ ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗]],
-  [[ ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║]],
-  [[ ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║]],
-  [[ ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║]],
-  [[ ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║]],
-  [[ ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝]],
-  [[]],
-  [[]],
-  [[]],
-  [[]],
-  [[]],
-  [[]],
-  [[]],
-  [[]],
+    [[]], [[]], [[]],
+    [[ ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗]],
+    [[ ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║]],
+    [[ ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║]],
+    [[ ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║]],
+    [[ ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║]],
+    [[ ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝]],
+    [[]], [[]], [[]], [[]], [[]], [[]], [[]], [[]]
 }
 
 vim.g.dashboard_custom_section = {
 
-  a = {description = {'  Find File          '}, command = 'Telescope find_files'},
-  d = {description = {'  Search Text        '}, command = 'Telescope live_grep'},
-  b = {description = {'  Recent Files       '}, command = 'Telescope oldfiles'},
-  c = {description = {'  New File           '}, command = 'NvimTreeOpen'},
-  e = {description = {'  Config             '}, command = 'edit ~/.config/nvim/init.lua'},
-  f = {description = {'ﮮ  Update Plugin      '}, command = 'PackerUpdate'},
-  g = {description = {'  Exit               '}, command = 'q'},
+    a = {
+        description = {'  Find File          '},
+        command = 'Telescope find_files'
+    },
+    e = {
+        description = {'  Search Text        '},
+        command = 'Telescope live_grep'
+    },
+    b = {
+        description = {'  Recent Files       '},
+        command = 'Telescope oldfiles'
+    },
+    c = {description = {'  New File           '}, command = 'NvimTreeOpen'},
+    d = {description = {'  File Browser       '}, command = 'RnvimrToggle'},
+    f = {
+        description = {'  Config             '},
+        command = 'edit ~/.config/nvim/init.lua'
+    },
+    g = {description = {'ﮮ  Update Plugin      '}, command = 'PackerUpdate'},
+    h = {description = {'  Exit               '}, command = 'q'}
 }
 
-
 vim.g.dashboard_custom_footer = {
-  [[]],
-  [[]],
-  [[]],
-  [[]],
-  [[]],
-  [[]],
-  [[]],
-  [[]],
-  [[]],
-  [[]],
-  'Hola. bienvenido.'
+    [[]], [[]], [[]], [[]], [[]], [[]], [[]], [[]], [[]], [[]],
+    'Hola. bienvenido.'
 }
