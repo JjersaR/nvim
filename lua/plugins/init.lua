49df4885eb5df1ac8a7e140c4b924dc774f00ef7
JjersaR <86206596+JjersaR@users.noreply.github.com>
2022-05-14 12:36:07 -0500

    Update init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -81,19 +81,6 @@ return require('packer').startup(function()
     -- ranger
     use 'kevinhwang91/rnvimr'
 
-    -- problemas
-    use {
-        "folke/trouble.nvim",
-        requires = "kyazdani42/nvim-web-devicons",
-        config = function()
-            require("trouble").setup {
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            }
-        end
-    }
-
     -- tema chido
     use({"catppuccin/nvim", as = "catppuccin"})
 
