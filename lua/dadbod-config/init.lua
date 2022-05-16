80dcd1fd9bc82ecf1da11cdfccb94d939239c0ad
JjersaR <86206596+JjersaR@users.noreply.github.com>
2022-05-16 07:48:14 -0500

    Create init.lua

---
+++ lua/dadbod-config/init.lua
@@ -0,0 +1,42 @@
+vim.g.db_ui_use_nerd_fonts = 1
+vim.g.db_ui_show_database_icon = 1
+vim.g.db_ui_force_echo_notifications = 1
+vim.g.db_ui_winwidth = 80
+
+vim.g.db_ui_table_helpers = {
+    mysql = {
+        Count = "select count(1) from {optional_schema}{table}",
+        Explain = "EXPLAIN {last_query}"
+    },
+    sqlite = {
+        Describe = "PRAGMA table_info({table})"
+    }
+}
+
+vim.g.db_ui_icons = {
+    expanded = {
+        db = '▾ ',
+        buffers = '▾ ',
+        saved_queries = '▾ ',
+        schemas = '▾ ',
+        schema = '▾ פּ',
+        tables = '▾ 藺',
+        table = '▾ ',
+    },
+    collapsed = {
+        db = '▸ ',
+        buffers = '▸ ',
+        saved_queries = '▸ ',
+        schemas = '▸ ',
+        schema = '▸ פּ',
+        tables = '▸ 藺',
+        table = '▸ ',
+    },
+    saved_query = '',
+    new_query = '璘',
+    tables = '離',
+    buffers = '﬘',
+    add_connection = '',
+    connection_ok = '✓',
+    connection_error = '✕',
+}
