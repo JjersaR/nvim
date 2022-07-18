46b9d0fade574f04ba3e0c38bdb204e36e77f4b1
JjersaR <86206596+JjersaR@users.noreply.github.com>
2022-07-18 09:12:18 -0500

    Update init.lua

--- lua/lenguajes/init.lua
+++ lua/lenguajes/init.lua
@@ -41,7 +41,10 @@ require'lspconfig'.jdtls.setup{
       if client.config.settings then
         client.notify('workspace/didChangeConfiguration', {settings = client.config.settings})
       end
-    end
+    end,
+  root_dir = function(fname)
+    return require'lspconfig'.util.root_pattern('pom.xml', 'gradle.build', '.git')(fname) or vim.fn.getcwd()
+   end
 }
 
 --sql
