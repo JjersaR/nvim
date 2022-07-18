ba153cdd695392356dbad029964847e511fd46cc
r <rikisj5649@gmail.com>
2022-07-18 12:01:56 -0500

    arreglando nvim-tree

--- lua/tree-config/init.lua
+++ lua/tree-config/init.lua
@@ -1,6 +1,6 @@
-vim.g.nvim_tree_highlight_opened_files = 1
 require'nvim-tree'.setup {
     actions = {open_file = {quit_on_open = true}},
     diagnostics = {enable = true},
-    git = {enable = true}
+    git = {enable = true},
+    renderer = {highlight_opened_files = "all"},
 }
