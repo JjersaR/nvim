2fe7cf8bacb0c3b1ae323cb56c39c592bd79b14a
r <rikisj5649@gmail.com>
2022-10-17 10:53:45 -0500

    solucionando error de capabilities en lenguajes

--- lua/lenguajes/init.lua
+++ lua/lenguajes/init.lua
@@ -1,4 +1,6 @@
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
 local lspFlags = {
   debounce_text_changes = 150,
 }
