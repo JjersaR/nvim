f3772f333cda2a147f1d1b876bb1a0c71bca0cde
r <rikisj5649@gmail.com>
2022-11-07 19:36:29 -0600

    Mejorando format desde nulls-ls

--- lua/nulls-ls-config/init.lua
+++ lua/nulls-ls-config/init.lua
@@ -21,7 +21,7 @@ local sources = {
 null_ls.setup({
   sources = sources,
   on_attach = function(client, bufnr)
-    if client.server_capabilities.documentFormattingProvider then
+    if client.server_capabilities.documentFormattingProvider or client.supports_method "textDocument/formatting" then
       vim.api.nvim_command [[augroup Format]]
       vim.api.nvim_command [[autocmd! * <buffer>]]
       vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
