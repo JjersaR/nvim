357a6c80782e9250e9a2b7aec5edc5f175c79aae
JjersaR <rikisj5649@gmail.com>
2023-01-30 13:21:08 -0600

    Mejorando keymaps

--- lua/tree-config/init.lua
+++ lua/tree-config/init.lua
@@ -1,5 +1,5 @@
 require 'nvim-tree'.setup {
-  actions = { open_file = { quit_on_open = true } },
+  -- actions = { open_file = { quit_on_open = true } },
   diagnostics = { enable = true },
   git = { enable = true },
   renderer = { highlight_opened_files = "all" },
