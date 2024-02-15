2ea40cb257f5b1c61e28cb830731a9c51782f672
Ricardo <rikisj5649@gmail.com>
2024-02-15 17:28:06 -0600

    Add python to config

--- after/plugin/format.lua
+++ after/plugin/format.lua
@@ -2,9 +2,10 @@ require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
 		java = { "google-java-format" },
+		python = { "black" },
 	},
-  format_on_save = {
-    lsp_fallback = true,
-    timeout_ms = 500,
-  }
+	format_on_save = {
+		lsp_fallback = true,
+		timeout_ms = 500,
+	},
 })
--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -8,6 +8,15 @@ require("mason").setup({
 	},
 })
 require("mason-lspconfig").setup({
-	ensure_installed = { "clangd", "google-java-format", "jdtls", "lua-language-server", "stylua", "pyright" },
+	ensure_installed = {
+		"clangd",
+		"google-java-format",
+		"jdtls",
+		"lua-language-server",
+		"stylua",
+		"pyright",
+		"black",
+		"flake8",
+	},
 	automatic_installation = false,
 })
