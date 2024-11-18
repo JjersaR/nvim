d3a1295c8b6413edafe53a68a8d2d5f9214cbb3a
JjersaR <86206596+JjersaR@users.noreply.github.com>
2024-11-18 16:29:38 -0600

    Update dashboard.lua

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,56 +1,54 @@
 return {
-  "goolord/alpha-nvim",
-  config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.dashboard")
-
-    -- Set header
-    dashboard.section.header.val = {
-      [[                                                   ]],
-      [[                                              ___  ]],
-      [[                                           ,o88888 ]],
-      [[                                        ,o8888888' ]],
-      [[                  ,:o:o:oooo.        ,8O88Pd8888"  ]],
-      [[              ,.::.::o:ooooOoOoO. ,oO8O8Pd888'"    ]],
-      [[            ,.:.::o:ooOoOoOO8O8OOo.8OOPd8O8O"      ]],
-      [[           , ..:.::o:ooOoOOOO8OOOOo.FdO8O8"        ]],
-      [[          , ..:.::o:ooOoOO8O888O8O,COCOO"          ]],
-      [[         , . ..:.::o:ooOoOOOO8OOOOCOCO"            ]],
-      [[          . ..:.::o:ooOoOoOO8O8OCCCC"o             ]],
-      [[             . ..:.::o:ooooOoCoCCC"o:o             ]],
-      [[             . ..:.::o:o:,cooooCo"oo:o:            ]],
-      [[          `   . . ..:.:cocoooo"'o:o:::'            ]],
-      [[          .`   . ..::ccccoc"'o:o:o:::'             ]],
-      [[         :.:.    ,c:cccc"':.:.:.:.:.'              ]],
-      [[       ..:.:"'`::::c:"'..:.:.:.:.:.'               ]],
-      [[     ...:.'.:.::::"'    . . . . .'                 ]],
-      [[    .. . ....:."' `   .  . . ''                    ]],
-      [[  . . . ...."'                                     ]],
-      [[  .. . ."'                                         ]],
-      [[ .                                                 ]],
-      [[                                                   ]],
-    }
-
-    -- Set menu
-    dashboard.section.buttons.val = {
-      dashboard.button("m", "  Find File", ":Telescope find_files<CR>"),
-      dashboard.button("t", "🌳 Files", ":Triptych<CR>"),
-      dashboard.button(
-        "e",
-        "🔎 Search by name",
-        "<cmd>lua require('telescope.builtin').grep_string({ search = vim.fn.input('Grep For > ')})<CR>"
-      ),
-      dashboard.button("r", "  Recent files", ":lua require'telescope.builtin'.oldfiles{}<CR>", {}),
-      dashboard.button("p", "✅ LazySync", ":Lazy sync<CR>"),
-      dashboard.button("s", "⚙️  Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-      dashboard.button("q", "🛑 Quit NVIM", ":qa<CR>"),
-    }
-
-    alpha.setup(dashboard.opts)
-
-    -- Disable folding on alpha buffer
-    vim.cmd([[
-    autocmd FileType alpha setlocal nofoldenable
-]])
-  end,
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    bigfile = { enabled = false },
+    bufdelete = { enabled = false },
+    notifier = { enabled = false },
+    dashboard = {
+      enabled = true,
+      preset = {
+        header = [[
+           _
+          | |
+ ___  __ _| |_ _   _ _ __ _ __
+/ __|/ _` | __| | | | '__| '_ \
+\__ \ (_| | |_| |_| | |  | | | |
+|___/\__,_|\__|\__,_|_|  |_| |_|
+        ]],
+      },
+      sections = {
+        { section = "header" },
+        { section = "keys", gap = 1, padding = 1 },
+        { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        {
+          pane = 2,
+          icon = " ",
+          title = "Git Status",
+          section = "terminal",
+          enabled = vim.fn.isdirectory(".git") == 1,
+          cmd = "git status --short --branch --renames",
+          height = 5,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
+        },
+        { section = "startup" },
+      },
+    },
+    debug = { enabled = false },
+    git = { enabled = false },
+    gitbrowse = { enabled = false },
+    lazygit = { enabled = false },
+    notify = { enabled = false },
+    quickfile = { enabled = false },
+    rename = { enabled = false },
+    statuscolumn = { enabled = false },
+    terminal = { enabled = false },
+    toggle = { enabled = false },
+    win = { enabled = false },
+    words = { enabled = false },
+  },
 }
