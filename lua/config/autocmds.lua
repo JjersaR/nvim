d7ae6131b92b2227662a11f640aba72a6456ff4c
JjersaR <rikisj5649@gmail.com>
2024-11-25 12:30:34 -0600

    Mejorando configuracion

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,3 +4,20 @@
 
 -- quitar espacios
 vim.cmd([[autocmd BufWritePre * :%s/\s\+$//e]])
+
+-- Autocomando para deshabilitar números en terminales
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "*",
+  callback = function()
+    vim.wo.number = false -- Desactiva los números normales
+    vim.wo.relativenumber = false -- Desactiva los números relativos
+  end,
+})
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "*",
+  callback = function()
+    -- Mapea Esc Esc para salir al modo normal en terminal
+    vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { buffer = 0, silent = true })
+  end,
+})
--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,4 +1,17 @@
 return {
   "akinsho/toggleterm.nvim",
   lazy = false,
+  config = function()
+    require("toggleterm").setup({
+      size = 20, -- Tamaño inicial del terminal
+      open_mapping = [[<Leader>;f]], -- Mapeo para abrir/ocultar la terminal
+      hide_numbers = true, -- Oculta los números de línea
+      shade_terminals = true, -- Sombrea el fondo de la terminal
+      shading_factor = 2, -- Nivel de sombreado
+      direction = "float", -- Tipo de terminal: "vertical" | "horizontal" | "float"
+      float_opts = {
+        border = "curved", -- Estilo del borde flotante
+      },
+    })
+  end,
 }
