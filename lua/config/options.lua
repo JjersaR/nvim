34e0211b121929b43912258068768d93b796a250
JjersaR <rikisj5649@gmail.com>
2025-02-17 15:14:29 -0600

    Agregando status de git a staline

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,11 @@ vim.opt.relativenumber = true
 vim.opt.scrolloff = 24
 vim.opt.autoread = true
 vim.opt.swapfile = false
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+  end,
+})
--- lua/plugins/staline.lua
+++ lua/plugins/staline.lua
@@ -21,6 +21,23 @@ return {
       local index = math.ceil(line_ratio * #chars)
       return chars[index]
     end
+    -- Función personalizada para obtener el estado de Git
+    local git_status = function()
+      local git_cmd = "git diff --shortstat 2> /dev/null"
+      local handle = io.popen(git_cmd)
+      local result = handle:read("*a")
+      handle:close()
+
+      if result == "" then
+        return ""
+      end
+
+      local added = tonumber(result:match("(%d+) insertions?")) or 0
+      local deleted = tonumber(result:match("(%d+) deletions?")) or 0
+      local modified = tonumber(result:match("(%d+) files? changed")) or 0
+
+      return string.format("+%d ~%d -%d", added, modified, deleted)
+    end
 
     local my_colors = { n = "#9CCFD8", i = "#9CCFD8", c = "#9CCFD8", v = "#9CCFD8", t = "#9CCFD8" }
 
@@ -32,7 +49,7 @@ return {
           { "StalineBranch", "branch" },
         },
         mid = { "lsp" },
-        right = { " ", "lsp_name", "  ", percentage },
+        right = { git_status, " ", "lsp_name", "  ", percentage },
       },
       defaults = {
         true_colors = true, -- LSP highlighing
